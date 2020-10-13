99029658ae69ee9bd9a95d442e81643dfb3c24aa
Gotar <eltahir.g@gmail.com>
2020-10-13 01:18:31 -0500

    working code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,15 @@
 // from data.js
-var tableData = data;
+let tableData = data;
 
 // YOUR CODE HERE!
+let table = d3.select("table-striped");
+let tbody = d3.select("tbody");
+
+tableData.forEach(function(ufoInfo) {
+    let row = tbody.append('tr');
+    Object.entries(ufoInfo).forEach(function([key, value]) {
+        let cell = row.append('td');
+        cell.text(value);
+    });
+
+});
