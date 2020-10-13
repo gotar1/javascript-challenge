085f6a16de2b0fec5acb5002d62317b591272a3d
Gotar <eltahir.g@gmail.com>
2020-10-13 04:52:20 -0500

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,13 +3,38 @@ let tableData = data;
 
 // YOUR CODE HERE!
 let table = d3.select("table-striped");
+let thead = d3.select("thead");
 let tbody = d3.select("tbody");
 
-tableData.forEach(function(ufoInfo) {
-    let row = tbody.append('tr');
-    Object.entries(ufoInfo).forEach(function([key, value]) {
-        let cell = row.append('td');
-        cell.text(value);
+
+
+
+tableData.forEach((ufoInfo) => {
+    // let row1 = thead.append('tr');
+    // let cell1 = row1.append('th');
+    // cell1.text(key);
+    let row2 = tbody.append('tr');
+    Object.entries(ufoInfo).forEach(([key, value]) => {
+        
+        let cell2 = row2.append('td');
+        
+        cell2.text(value);
     });
 
 });
+
+let button = d3.select('#filter-btn');
+
+
+button.on("click", runEnter);
+table.on("submit", runEnter);
+
+function runEnter() {
+    d3.event.preventDefault();
+    let inputField = d3.select('#datetime');
+    let inputValue = inputField.property('value');
+    let filteredData = tableData.filter(date => date.datetime === inputValue);
+    table.html("");
+    table.append(filteredData).text()
+
+};
