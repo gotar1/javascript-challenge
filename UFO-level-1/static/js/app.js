7080c6f5d0a909935c55ce7affccd11d8bd2b3ff
Gotar <eltahir.g@gmail.com>
2020-10-14 00:57:29 -0500

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,23 +2,28 @@
 let tableData = data;
 
 // YOUR CODE HERE!
-let table = d3.select("table-striped");
-let thead = d3.select("thead");
+let table = d3.select(".table-striped");
 let tbody = d3.select("tbody");
+
+let thead = d3.select("thead");
+// thead.html("");
+// let headerRow = thead.append('tr');
+// headerRow.append('th').text(Date);
+// headerRow.append('th').text(City);
+// headerRow.append('th').text(State);
+// headerRow.append('th').text(Country);
+// headerRow.append('th').text(Shape);
+// headerRow.append('th').text(Duration);
+// headerRow.append('th').text(Comments);
 
 
 function tableInfo(ufoInfo) {
     tbody.html("");
-    ufoInfo.forEach((sighting) => {
-        // let row1 = thead.append('tr');
-        // let cell1 = row1.append('th');
-        // cell1.text(key);
-        let row2 = tbody.append('tr');
-        Object.entries(sighting).forEach(([key, value]) => {
-            
-            let cell2 = row2.append('td');
-            
-            cell2.text(value);
+    ufoInfo.forEach((ufoSighting) => {
+        let row = tbody.append('tr');
+        Object.entries(ufoSighting).forEach(([key, value]) => {      
+            let tableBody = row.append('td');
+            tableBody.text(value);
         });
     
     });
@@ -26,21 +31,25 @@ function tableInfo(ufoInfo) {
 }
 tableInfo(tableData);
 
-let correctDate = d3.select('.text-center');
+let correctDate = d3.select('#date-correct');
 let button = d3.select('#filter-btn');
+let inputField = d3.select('#datetime')
 
-button.on("click", runEnter);
-inputField.on("change", runEnter);
+button.on("click", filterTable);
+// inputField.on("submit", filterTable);
 
-function runEnter() {
+function filterTable() {
     d3.event.preventDefault();
     let inputField = d3.select('#datetime');
     let inputValue = inputField.property('value');
     let filteredTable = tableData.filter(date => date.datetime === inputValue);
     if(typeof filteredTable === "undefined" || filteredTable === null) {
         let filteredTable = tableData;
-        let correction = correctDate.append('p')
-        correction.text("Date out of range, please input correct date..");
+        tableInfo(filteredTable);
+        correctDate.html("");
+        // let correction = correctDate.append('p');
+        correctDate.text("Date out of range, please input correct date..");
+        console.log(filteredTable)
     }
     tableInfo(filteredTable);
 
