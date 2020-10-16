ef82c3537f1ff0a2692fa5149937c0a27ce28cc6
Gotar <eltahir.g@gmail.com>
2020-10-15 21:26:01 -0500

    final code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,12 +1,13 @@
 // from data.js
 let tableData = data;
 
-// YOUR CODE HERE!
+// using d3 liberary to select my HTML classes and id..
 let table = d3.select(".table-striped");
 let tbody = d3.select("tbody");
-
 let thead = d3.select("thead");
 
+// build function create table row and table data in table body and populate them..
+// using data from tableData..
 function tableInfo(ufoInfo) {
     tbody.html("");
     ufoInfo.forEach((ufoSighting) => {
@@ -14,20 +15,27 @@ function tableInfo(ufoInfo) {
         Object.entries(ufoSighting).forEach(([key, value]) => {      
             let tableBody = row.append('td');
             tableBody.text(value);
-        });
-    
+        });   
     });
+};
 
-}
+// run above function to populate table body using provided data...
 tableInfo(tableData);
 
+// this part for filter..we gonna filter by time..
+// select where my input data is gonna be stored in the HTML and where the search button is..
+// included a paragraph to display error if user entered wrong date..
 let errDisplay = d3.select('#date-correct');
 let button = d3.select('#filter-btn');
 let inputField = d3.select('#datetime')
 
+// button click handle to run the filter function when ever search button is clicked...
+// included input submssion is one need to override search button..
 button.on("click", filterTable);
 // inputField.on("submit", filterTable);
 
+// create filter function to filter by time...
+// include an if statement to handle error entry by user..
 function filterTable() {
     d3.event.preventDefault();
     errDisplay.text('');
@@ -40,17 +48,14 @@ function filterTable() {
     errDisplay.text("Did you find what you looking for?? Keep searching..");
 
     if(filteredTable.length == 0) {
-        // let filteredTable = tableData;
         tableInfo(tableData);
         errDisplay.html("");
         errDisplay.text("Date out of range, please input correct date..");
         console.log(errDisplay);
     }
     else {
-
         tableInfo(filteredTable);
     }
-
 };
 
 
