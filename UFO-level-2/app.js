31d43d442b3b789f6f83d1c03f482365e48e60c9
Gotar <eltahir.g@gmail.com>
2021-01-23 01:24:42 -0600

    Final Revised Codes

--- UFO-level-2/app.js
+++ UFO-level-2/app.js
@@ -1,16 +1,21 @@
 // from data.js
 let tableData = data;
 
-// using d3 liberary to select my HTML classes and id.. 
-let table = d3.select(".table-striped");
+// using d3 liberary to select my HTML class and id.. 
 let tbody = d3.select("tbody");
-let thead = d3.select("thead");
 
 // create function to populate table data..
-function tableInfo(ufoInfo) {
+function buildTable(ufoInfo) {
+    // First, clear out any existing data
     tbody.html("");
+
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
     ufoInfo.forEach((ufoSighting) => {
         let row = tbody.append('tr');
+
+        // Loop through each field in the dataRow and add
+        // each value as a table cell (td)
         Object.entries(ufoSighting).forEach(([key, value]) => {      
             let tableBody = row.append('td');
             tableBody.text(value);
@@ -18,60 +23,60 @@ function tableInfo(ufoInfo) {
     });
 }
 
-// run above function to populate table body using provided data...
-tableInfo(tableData);
+// Keep Track of all filters
+let filters = {};
 
-// this part for filter..we gonna filter by multi inputs..
-// select where my input data is gonna be stored in the HTML and where the search button is..
-// included a paragraph to display error if user entered wrong date..
+function updateFilters() {
 
+  // Save the element, value, and id of the filter that was changed
+  let changedElement = d3.select(this).select("input");
+  let elementValue = changedElement.property("value");
+  let filterId = changedElement.attr("id");
+
+  // If a filter value was entered then add that filterId and value
+  // to the filters list. Otherwise, clear that filter from the filters object
+  if (elementValue) {
+    filters[filterId] = elementValue;
+  }
+  else {
+    delete filters[filterId];
+  }
+
+  // Call function to apply all filters and rebuild the table
+  filterTable();
+
+}
+
+// select element to display error message
 let errDisplay = d3.select('#date-correct');
-let dateInputField = d3.select('#datetime-filter');
-let cityInputField = d3.select('#city-filter');
-let stateInputField = d3.select('#state-filter');
-let countryInputField = d3.select('#country-filter');
-let shapeInputField = d3.select('#shape-filter');
-
-// input change handle to run the filter function when ever new search criteria is entered...
-dateInputField.on("change", filterTable);
-cityInputField.on("change", filterTable);
-stateInputField.on("change", filterTable);
-countryInputField.on("change", filterTable);
-shapeInputField.on("change", filterTable);
-
-// create function to filter data based on user input
 function filterTable() {
-    d3.event.preventDefault();
-    let inputValueDate = dateInputField.property('value');
 
-    
-    let inputValueCity = cityInputField.property('value');
- 
+    // Set the filteredData to the tableData
+    let filteredData = tableData;
 
-    let inputValueState = stateInputField.property('value');
-    let inputValueCountry = countryInputField.property('value');
-    let inputValueShape = shapeInputField.property('value');
-
-    let filteredTable = tableData.filter(item => {
-        return (
-            item.datetime === inputValueDate ||
-            item.city === inputValueCity || 
-            item.state === inputValueState ||
-            item.country === inputValueCountry ||
-            item.shape === inputValueShape
-        );
+    // Loop through all of the filters and keep any data that
+    // matches the filter values
+    Object.entries(filters).forEach(([key, value]) => {
+        filteredData = filteredData.filter(row => row[key] === value);
     });
-    
-    if(filteredTable.length == 0) {
-        tableInfo(tableData);
+
+    // prevent default and clear error holder text if any
+    d3.event.preventDefault();
+    errDisplay.text('');
+
+    // display error message for incorrect data, else populate table
+    if(filteredData.length == 0) {
+        buildTable(tableData);
         errDisplay.html("");
         errDisplay.text("Data out of range, please input correct data..");
-    }
+        }
     else {
-        tableInfo(filteredTable);
-    }  
-};
-function update() {
-    let filteredTable = tableData().filter(filterTable);
-    };
-update();
+        buildTable(filteredData);
+        }  
+}
+
+// Attach an event to listen for changes to each filter
+d3.selectAll(".filter").on("change", updateFilters);
+
+// Build the table when the page loads
+buildTable(tableData);
